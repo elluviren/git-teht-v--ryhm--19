17201036844a51547a15480307173cc2e1910ffe
Elias Rosenberg <elias.rosenberg@student.hamk.fi>
2024-12-11 22:19:44 +0200

    Aloitettu kokeilulla.
    Aloitettu tehtävä.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Ryhmätehtävä");
+    
+    
+    
+    }
+
+
+
+
+}
+class swimPool {
+
+    public int width;
+    public int length;
+
+    public Pool(int w, int l);
+    {
+        width = w;
+        length = l;
+
     }
 }
+ 
