d06119c8f95b3dd2951334a27d87f446d08257b5
amk1005251 <jani.laakso@student.hamk.fi>
2024-12-15 20:02:45 +0200

    Jani lisäsi Ympyra -luokan ja sen pinta-alan laskennan käyttäjän antaman syötteen perusteella

--- src/App.java
+++ src/App.java
@@ -1,14 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Ryhmätehtävä");
-    
-    
+
+        // Jani lisäsi käyttäjän syötteen kyselyn ja
+        // Ympyra luokan kutsumisen ja ympyrän pinta-alan laskennan
+        // muokkailkaa vapaasti alla olevaa koodia tarpeiden mukaan
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Anna ympyrän säde");
+        double sade = Double.parseDouble(input.nextLine());
+
+        Ympyra ympyraP = new Ympyra(sade);
+        ympyraP.laskePintaAla();
+
+        input.close();
     
     }
-
-
-
-
 }
 class swimPool {
 
@@ -21,5 +31,19 @@ class swimPool {
         length = l;
 
     }
+}
+
+// Janin lisäämä Ympyrä luokka ja ympyrän pinta-alan laskenta
+class Ympyra {
+	private double sade;
+
+	public Ympyra(double sade) {
+        this.sade = sade;
+	}
+	
+    public void laskePintaAla() {
+        double tulos =  3.14159 * (sade * sade);
+        System.out.printf("Ympyrän pinta-ala on: " + "%.2f%n", tulos);
+	}
 }
  
